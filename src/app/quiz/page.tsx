ab94c6351892b027e3662312a8232b8aea1dbd6b
Pat Zdziabek <pat.zdziabek@gmail.com>
2025-02-06 11:23:21 +0100

    Czy to jeszcze działa?

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,10 +1,15 @@
+'use client';
+
+import { Suspense } from 'react';
 import Quiz from "../quiz/components/Quiz";
 
 export default function QuizPage() {
   return (
     <div className="grid grid-rows-[1fr] sm:grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-8 sm:pb-20 sm:gap-16 sm:p-20">
       <main className="w-full h-full sm:w-auto sm:h-auto flex flex-col gap-8 row-start-2 items-center">
-        <Quiz />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Quiz />
+        </Suspense>
       </main>
     </div>
   );
