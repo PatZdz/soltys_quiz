495cc31724aaa106d6e1b1767de7d529f9e0e363
Pat Zdziabek <pat.zdziabek@gmail.com>
2025-03-15 13:25:00 +0100

    Update page.tsx

--- src/app/range/page.tsx
+++ src/app/range/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Suspense } from 'react';
 import QuestionRangeSelector from '../range/components/QuestionRangeSelector';
 
 export default function RangePage() {
@@ -7,7 +8,9 @@ export default function RangePage() {
     <div className="min-h-screen flex flex-col">
       <main className="flex-1 flex flex-col items-center justify-center w-full p-0 sm:p-20">
         <div className="w-full sm:max-w-[672px]">
-          <QuestionRangeSelector />
+          <Suspense fallback={<div>Loading...</div>}>
+            <QuestionRangeSelector />
+          </Suspense>
         </div>
       </main>
     </div>
