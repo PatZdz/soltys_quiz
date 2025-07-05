d524e6b4503effa114b756db7327633ffe0a0562
Pat Zdziabek <159633087+PatZdz@users.noreply.github.com>
2025-07-05 20:26:01 +0200

    Update questions2.ts

--- src/data/questions2.ts
+++ src/data/questions2.ts
@@ -827,8 +827,7 @@ export const questionRanges: QuestionRange[] = [
   { id: 4, label: "Pytania 31 - 40", startId: 31, endId: 40 },
   { id: 5, label: "Pytania 41 - 50", startId: 41, endId: 50 },
   { id: 6, label: "Pytania 51 - 60", startId: 51, endId: 60 },
-  { id: 7, label: "Pytania 61 - 70", startId: 61, endId: 70 },
-  { id: 8, label: "Pytania 71 - 73", startId: 71, endId: 73 }
+  { id: 7, label: "Pytania 61 - 70", startId: 61, endId: 73 },
 ];
 
 export const getQuestionsFromRanges = (selectedRangeIds: number[]): QuizQuestion[] => {
